7a5b526a224d4821f40de8122f7346c355e8b522
Petrov Anton <gg4every@gmail.com>
2019-01-30 15:58:14 +0400

    [LIC] Update java-code copyright

--- app/src/main/java/website/petrov/browser/browser/NinjaGestureListener.java
+++ app/src/main/java/website/petrov/browser/browser/NinjaGestureListener.java
@@ -1,3 +1,26 @@
+/*
+ * Open source android application.
+ *
+ * Copyright (c) 2015 Matthew Lee
+ * Copyright (c) 2017 Gaukler Faun
+ * Copyright (c) 2019 Petrov Anton
+ *
+ * This file is part of Suze Browser.
+ *
+ * Suze Browser is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Suze Browser is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Suze Browser. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package website.petrov.browser.browser;
 
 import android.view.GestureDetector;
